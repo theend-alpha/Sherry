a4666da1fc364649c48ac151e1d6d82109c38b88
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-04-25 14:13:11 +0530

    Update mongo.py

--- Yuriko/mongo.py
+++ Yuriko/mongo.py
@@ -3,13 +3,13 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from Yuriko import MONGO_DB_URI
-from Yuriko.confing import get_int_key, get_str_key
+from Sherry import MONGO_DB_URI
+from Sherry.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "Yuriko"
+MONGO_DB = "Sherry"
 
 
 client = MongoClient()
